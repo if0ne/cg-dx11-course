b3f9b260cb08313cf7e01626c1ef4779612cb437
Pavel Agafonov <pavel.agafonov.al@gmail.com>
2024-03-27 12:54:11 +0300

    Add: materials

--- shaders/KatamariLight.hlsl
+++ shaders/KatamariLight.hlsl
@@ -9,16 +9,31 @@ struct SModelData
     float4x4 Transform;
 };
 
-struct Directional
+struct SDirectional
 {
     float4 Direction;
     float4 Color;
 };
 
-struct Point
+struct SPoint
 {
     float3 Pos;
     float3 Color;
+};
+
+struct SMaterial
+{
+    float4 BaseColor;
+    float Reflection;
+    float Absorption;
+    float Shininess;
+    float _padding;
+};
+
+struct SAmbient
+{
+    float3 Color;
+    float Intensity;
 };
 
 // VS
@@ -33,9 +48,19 @@ cbuffer Model : register(b1)
 }
 
 // PS
-cbuffer DirCB : register(b2)
+cbuffer Directional : register(b2)
 {
-    Directional directional;
+    SDirectional Directional;
+};
+
+cbuffer Ambient : register(b3)
+{
+    SAmbient Ambient;
+}
+
+cbuffer Material : register(b8)
+{
+    SMaterial Material;
 };
 
 Texture2D Texture : TEXTURE : register(t0);
@@ -51,6 +76,7 @@ struct VS_IN
 struct PS_IN
 {
     float4 pos : SV_POSITION;
+    float3 worldPos : POSITION;
     float3 normal : NORMAL;
     float2 tex : TEXCOORD;
 };
@@ -62,25 +88,28 @@ PS_IN VSMain(VS_IN input)
     output.pos = mul(float4(input.pos, 1.0f), mul(ModelData.Transform, CameraData.WorldViewProj));
     output.tex = input.tex;
     output.normal = normalize(mul(float4(input.normal, 0), ModelData.Transform).xyz);
+    output.worldPos = mul(float4(input.pos, 1.0f), ModelData.Transform);
 	
     return output;
 }
 
 float3 CalcDirLight(float3 normal, float3 viewDir)
 {
-    float3 lightDir = normalize(-directional.Direction.xyz);
+    float3 lightDir = normalize(-Directional.Direction.xyz);
 
-    float diff = max(dot(normal, lightDir), 0.0);
+    float diff = Directional.Color.w * Material.Reflection * max(dot(normal, lightDir), 0.0);
 
     float3 reflectDir = reflect(-lightDir, normal);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 1.0);
+    float spec = Material.Absorption * pow(max(dot(viewDir, reflectDir), 0.0), Material.Shininess);
+    
+    float3 amb = Ambient.Color * Ambient.Intensity;
 
-    return directional.Color.w * (diff + spec) * directional.Color.xyz;
+    return (diff + spec) * amb * Directional.Color.xyz;
 }
 
 float4 PSMain(PS_IN input) : SV_Target
 {
-    float3 surfaceColor = Texture.Sample(Sampler, input.tex);
+    float3 surfaceColor = Material.BaseColor.xyz * Texture.Sample(Sampler, input.tex);
     float3 dirLight = CalcDirLight(input.normal, CameraData.ViewPos);
     float3 finalColor = surfaceColor * dirLight;
     return float4(finalColor, 1.0);
